1d75308b8da6f4f3b3be71c59b6cfafe174458d3
Deviant-Code <ericksenjesse@gmail.com>
2018-02-02 12:28:25 -0800

    updated library.java

--- Book.java
+++ Book.java
@@ -1,14 +1,22 @@
+/* Jesse Ericksen
+   CSCI 241
+   Lab #2
+   
+   This provides the book object, to be used with Library.java to store Books. */
+
 public class Book {
 
-  public int isbn;
-  //seprate author into three variables
-  public String author;
-  public String title;
-  public String publisher;
-  public int year;
-  public int edition;
+  private String isbn;
+  private String author;
+  private String title;
+  private String publisher;
+  private int year;
+  private int edition;
 
-  public Book(int isbn, String author, String title, String publisher, int year, int edition){
+
+  //Constructs a book object with given data.
+  public Book(String isbn, String author, String title, String publisher, int year, int edition){
+  
     this.isbn = isbn;
     this.author = author;
     this.title = title;
@@ -18,40 +26,11 @@ public class Book {
 
   }
 
-  public Book(){
-    this(0, null, null, null, 0, 0);
-  }
-
+  //Return a string representation for book.
   public String toString(){
-      return String.format("ISBN: %d%nAuthor: %s%nTitle: %s%nPublisher: %s%nYear: %d%nEdition: %d",
+      return String.format("ISBN: %s%nAuthor: %s%nTitle: %s%nPublisher: %s%nYear: %d%nEdition: %d",
                      this.isbn, this.author, this.title, this.publisher, this.year, this.edition);
   }
-
-  public void author(String author){
-    this.author = author;
-  }
-
-  public void isbn(int isbn){
-    this.isbn = isbn;
-  }
-
-  public void title(String title){
-    this.title = title;
-  }
-
-  public void publisher(String publisher){
-    this.publisher = publisher;
-  }
-
-  public void year(int year){
-    this.year = year;
-  }
-
-  public void edition(int edition){
-    this.edition = edition;
-  }
-
-
 
 
 
